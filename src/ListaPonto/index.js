80c663cc60599453ca841bfe7186098519682af3
Felipe <felipe-daros@hotmail.com>
2022-08-02 22:39:06 -0300

    Lista

--- src/ListaPonto/index.js
+++ src/ListaPonto/index.js
@@ -1,11 +1,13 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useState } from "react";
-import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity, Image } from "react-native";
 import CrudService from '../services/crudService';
+import iconeRefresh from '../../assets/refresh-icon-10853.png';
 
 export default function RondaListaPonto() {
 
   const [data, setData] = useState('');
+  const [dados, setDados] = useState('');
 
   
 
@@ -21,28 +23,40 @@ export default function RondaListaPonto() {
       user_id: id
     }).then((d) => {
       setData(d.data);
-      console.log(d.data)
     }).catch(e => {
       console.log(e);
     });
+    t.map((e) => {
+      Object.values(e).forEach(item =>{
+        setDados(item)
+        console.log(dados.user_id);
+      });
+    })
 
     return
   }
 
-  function keys(){
-    const {user_id} = t;
-    console.log(user_id)
+  function teste(){
+    console.log(dados);
   }
+
 
   return (
     <View style={styles.container}>
       
-      <TouchableOpacity onPress={findUser} style={{margin:20}}>
-        <Text>TETETE</Text>
+      <TouchableOpacity onPress={findUser} style={{margin:30}}>
+        <Image 
+          source={iconeRefresh}
+          style={{height: 70, width: 150}}
+        />
       </TouchableOpacity>
-      <TouchableOpacity onPress={keys}>
-        <Text>b</Text>
+      <TouchableOpacity onPress={teste} style={{margin:30}}>
+        <Image 
+          source={iconeRefresh}
+          style={{height: 70, width: 150}}
+        />
       </TouchableOpacity>
+      <Text>{dados.id}</Text>
     </View>
   )
 }

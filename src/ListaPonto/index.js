31eead0d5729f8d21d9e47dc95683a43befe7e54
FelipeDaros <felipe-daros@hotmail.com>
2022-08-03 16:59:09 -0300

    List

--- src/ListaPonto/index.js
+++ src/ListaPonto/index.js
@@ -1,21 +1,23 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useState } from "react";
-import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity, Image } from "react-native";
+import { useEffect, useState } from "react";
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity, Image, Alert } from "react-native";
 import CrudService from '../services/crudService';
 import iconeRefresh from '../../assets/refresh-icon-10853.png';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import IconeOff from '../../assets/icons8-off-47.png';
+import IconeOn from '../../assets/icons8-on-47.png';
+
 
 export default function RondaListaPonto() {
-
   const [data, setData] = useState('');
   const [dados, setDados] = useState([]);
-
-  
-
   const t = [data];
+   
+  useEffect(() => {
+    roundsUser();
+  },[]);
 
-  
-  
-  async function findUser() {
+  const roundsUser = async() => {
     const crudService = new CrudService
     const id = await AsyncStorage.getItem("id");
 
@@ -23,35 +25,61 @@ export default function RondaListaPonto() {
       user_id: id
     }).then((d) => {
       setData(d.data);
+      //console.log(d.data)
     }).catch(e => {
       console.log(e);
     });
     t.map((e) => {
       Object.values(e).forEach(item =>{
-        setDados(item)
-        console.log(dados);
-      });
-    })
+        setDados(item);
+    });
+    })};
 
-    return
-  }
 
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity style={styles.card}>
+      <View style={styles.viewCard}>
+        <View>
+          <Text style={styles.itemTexto}>Local: {item.point_id.locale}</Text>
+          <Text style={styles.itemTexto}>Responsável: {item.user_id.name}</Text>
+        </View>
+        {item.stats == true ? 
+        <Image 
+        source={IconeOff}
+        style={styles.iconeStats}
+        />
+        :
+        <Image 
+          source={IconeOn}
+          style={styles.iconeStats}
+        />
+        }
+      </View>
+    </TouchableOpacity>
+  );
 
   return (
     <View style={styles.container}>
-      
-      <TouchableOpacity onPress={findUser} style={{margin:30}}>
-        <Image 
-          source={iconeRefresh}
-          style={{height: 70, width: 150}}
-        />
-      </TouchableOpacity>
+      <Text style={styles.textoTitulo}>LUGARES DA SUA RONDA</Text>
       {dados <= (dados.length == 0) 
-      ? <Text>Vazio!</Text>
-      : <View key={dados.id}>
-          <Text>{dados.id+' '+dados.data+' '+dados.stats}</Text>
+      ? <TouchableOpacity onPress={roundsUser} style={styles.botaoAtualizar}>
+          <Text style={styles.textoAtualizar}>Atualizar</Text>
+          <Image
+            source={iconeRefresh}
+            style={styles.icone}
+          />
+        </TouchableOpacity>
+      : <View style={styles.viewFlatList}>
+        <FlatList 
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+          style={{height: 250}}
+          />
         </View>
       }
+      
     </View>
   )
 }
@@ -63,5 +91,55 @@ const styles = StyleSheet.create({
     backgroundColor: '#d13f3f',
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  textoTitulo:{
+    fontSize: 20, 
+    color: '#fff', 
+    fontWeight: 'bold', 
+    margin: 25
+  },
+  viewFlatList: {
+    height: 400
+  },
+  card:{
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    width: 300,
+    height: 100,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 20
+  },
+  viewCard: {
+    flexDirection: 'row', 
+    alignItems: 'center'
+  },
+  itemTexto: {
+    margin: 5,
+    color: '#fff',
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  botaoAtualizar: {
+    width: 100,
+    height: 60,
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    alignItems: 'center',
+    borderRadius: 5
+  },
+  textoAtualizar: {
+    fontSize: 18,
+    color: '#000',
+    fontWeight: 'bold'
+  },
+  icone: {
+    width: 60,
+    height: 40
+  },
+  iconeStats: {
+    width: 35,
+    height: 35,
+    margin: 5
   }
 });
